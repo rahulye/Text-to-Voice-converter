55e3162f565179c1f9ed599547834227ff7dbb1f
Rahul-krishh <rahulmykris@gmail.com>
2025-11-22 19:31:07 +0530

    feat : make async function logic

--- script.js
+++ script.js
@@ -1,23 +1,54 @@
 let speech = new SpeechSynthesisUtterance();
 
-//to select the language and speak
-let voices = [];  //creating an array to get all voices and to store 
-let voiceSelected = document.querySelector('.js-select');
-
-window.speechSynthesis.onvoiceschanged = () => {
-  voices = window.speechSynthesis.getVoices();
-  speech.voice = voices[0]; // default is stored here
-
-  voices.forEach( (voice,i) => {
-    (voiceSelected.options[i]) = new Option( voice.name , i );
-  });
-};
-
-// to speak
 document.querySelector('.js-btn').addEventListener( 'click' , () => {
   speech.text = document.querySelector('.js-textarea').value;
-  window.speechSynthesis.speak(speech);  
-})
+  window.speechSynthesis.speak(speech);
+});
+
+let voices = [];
+let voiceSelected = document.querySelector('.js-select');
+async function initSpeech() {  
+  
+  // 1.get the voices after await(bring promise) 
+  let voices = await loadSpeech();  
+  // 2.set the default voice
+  speech.voice = voices[0];
+  // 3.show the voices in the page 
+  voices.forEach( ( voice , i) => {
+    voiceSelected.options[i] = new Option( voice.name , i);
+    voices[i] = voiceSelected.options[i];
+  });
+  console.log(voices);
+  console.log('2');
+};
+
+initSpeech().then( () => {
+  console.log('3');
+});
+
+function loadSpeech() {  
+  const returnValue = new Promise( (resolve) => {
+    window.speechSynthesis.onvoiceschanged = () => {
+      voices = window.speechSynthesis.getVoices();
+      resolve(voices)
+      console.log('1');
+    };
+  });
+  return returnValue;
+};
+
+
+
+
+
+
+//---->this alone is enough to read the text but to choose different we need more code so yeah
+// let speech = new SpeechSynthesisUtterance();
+// document.querySelector('.js-btn').addEventListener( 'click' , () => {
+//   speech.text = document.querySelector('.js-textarea').value;
+//   window.speechSynthesis.speak(speech);
+// });
+
 
 
 // let speech = new SpeechSynthesisUtterance();
